37c79fb41b42a29bc074a21c0c37c0a9ef0dc98c
Dorian-Ferreira <dorian.ferreira42@gmail.com>
2024-03-11 13:12:03 +0100

    Champion Class with search on click and Resource

--- src/app/components/champion/champions/champions.component.ts
+++ src/app/components/champion/champions/champions.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ChampionMiniComponent } from "../champion-mini/champion-mini.component";
 import { ApiService } from '../../../../service/api-service.service';
 import { Champion } from '../../../../entity/champion';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
+    
 @Component({
     selector: 'app-champions',
     standalone: true,
@@ -11,7 +13,7 @@ import { CommonModule } from '@angular/common';
     styleUrl: './champions.component.css',
     imports: [ChampionMiniComponent, CommonModule]
 })
-export class ChampionsComponent {
+export class ChampionsComponent implements OnInit {
 
   allChampions!:Array<Champion>;
   champions!:Array<Champion>;
@@ -19,23 +21,36 @@ export class ChampionsComponent {
 
   filter!:string;
 
-  constructor(private apiService: ApiService) {
+  constructor(private apiService: ApiService, private route: ActivatedRoute) {
     this.allChampions = new Array<Champion>;
     this.champions = new Array<Champion>;
+  }
+  
+  ngOnInit(): void {
     this.apiService.versions.latest().then(data => this.version = data);
 
     this.apiService.champion.all().then(data => {
       for(const champion in data.data) {
         this.allChampions.push(data.data[champion]);
       }
-    });
 
-    this.champions = this.allChampions;
+      this.champions = this.allChampions;
+  
+      this.route.queryParams.subscribe(params => {
+        if(params["search"]){
+          this.filterChampionsList(params["search"].toLowerCase());
+        }
+      });
+    });
   }
 
   onKey(event: any) {
-    this.champions = this.allChampions.filter((c) => c.name.toLowerCase().includes(event.target.value.toLowerCase())
-                                                || c.tags.filter((t) => t.toLowerCase().includes(event.target.value.toLowerCase())).length > 0
+    this.filterChampionsList(event.target.value.toLowerCase());
+  }
+
+  filterChampionsList(search:string) {
+    this.champions = this.allChampions.filter((c) => c.name.toLowerCase().includes(search)
+                                                || c.tags.filter((t) => t.toLowerCase().includes(search)).length > 0
                                               );
   }
 }
